6dac5c8650f27e1b8c65d9ccd80534cdaf57ed1c
heming-jd <yhm050227@qq.com>
2025-09-25 13:18:34 +0800

    1

--- spring-jdbc-project2/src/test/sql/node.sql
+++ spring-jdbc-project2/src/test/sql/node.sql
@@ -10,11 +10,3 @@ SELECT descendant_id ,level
 FROM node_closure
 WHERE ancestor_id= 'node_001'
 ORDER BY level DESC;
-
-# 查询所有子节点
-explain
-SELECT n.*, nc.level as depth
-FROM node n
-         INNER JOIN node_closure nc ON n.id = nc.descendant_id
-WHERE nc.ancestor_id = 'node_001'  -- 指定节点ID
-ORDER BY nc.level, n.node_name;
